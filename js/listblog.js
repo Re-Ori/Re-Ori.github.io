083fd48553af9e821eba8e58e518e9f6a680ed8d
Origin <328535835@qq.com>
2024-10-05 21:16:34 +0800

    Update 0.0.5

---
+++ js/listblog.js
@@ -0,0 +1,144 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get('json')) {
+        var json_url = urlParams.get('json');
+        const leftPanel = document.getElementsByClassName('left-panel')[0];
+        console.log(leftPanel);
+        const back = document.createElement('a');
+        back.id = "back";
+        back.innerText = "返回";
+        back.href = "index.html"
+        leftPanel.appendChild(back);
+    } else {
+        var json_url = 'json/blog.json';
+    }
+
+    fetch(json_url)
+        .then(response => response.json())
+        .then(data => {
+            const blogContent = document.getElementById('blog-content');
+
+            search = urlParams.get('search')
+            if (search) {
+                blogContent.innerHTML = `<h2>BLOG列表</h2>
+                <input type="text" id="searchInput" placeholder="搜索Blog">
+                <button id="searchButton" style="margin-bottom:10px;margin-left:10px;">搜索</button>
+                <button id="clearSearch" style="margin-left:10px;">清除搜索</button>`;
+                searchInput.value = search;
+                clearSearch.addEventListener('click', () => {
+                    window.location.search = `json=${json_url}`;
+                });
+
+            } else {
+                blogContent.innerHTML = `<h2>BLOG列表</h2>
+                <input type="text" id="searchInput" placeholder="搜索Blog">
+                <button id="searchButton" style="margin-bottom:10px;margin-left:10px;">搜索</button>`;
+            }
+            searchInput.addEventListener('keyup', (event) => {
+                if (event.key === 'Enter') {
+                    window.location.search = `json=${json_url}&search=${encodeURIComponent(searchInput.value.trim())}`;
+                }
+            });
+            searchButton.addEventListener('click', () => {
+                window.location.search = `json=${json_url}&search=${encodeURIComponent(searchInput.value.trim())}`;
+            });
+
+            data.blogs.forEach(blog => {
+                if (search) {
+                    const regex = new RegExp(`(${search})`, 'gi'); // 不区分大小写的正则表达式
+                    if (!(String(blog.title).includes(search) || String(blog.preview).includes(search))) {
+                        return; //跳出本次forEach循环
+                    } else {
+                        //将搜索关键字突出显示
+                        if (String(blog.title).includes(search)) { blog.title = blog.title.replace(regex, '<span style="background-color: #98cdff;border: 1px solid #399fff;">\$1</span>'); }
+                        if (String(blog.preview).includes(search)) { blog.preview = blog.preview.replace(regex, '<span style="background-color: #98cdff;border: 1px solid #399fff;">\$1</span>'); }
+                    }
+                }
+                const blogPost = document.createElement('div');
+                blogPost.classList.add('blog-post');
+                blogPost.id = `blog-${blog.id}`;
+
+                if (("coverImage" in blog) && ("preview" in blog)) {
+                    blogPost.innerHTML = `
+                        <h3>${blog.title}</h3>
+                        <p style="color:#444444;">${blog.preview}</p>
+                        <img src="${blog.coverImage}" alt="${blog.coverImage}" style="max-height: 200px; min-width:100px; height: auto;"/>
+                        <p style="color:#999999;">${blog.date}</p>
+                    `;
+                } if (!("coverImage" in blog) && ("preview" in blog)) {
+                    blogPost.innerHTML = `
+                        <h3>${blog.title}</h3>
+                        <div style="color:#444444;">${blog.preview}</div>
+                        <p style="color:#999999;">${blog.date}</p>
+                    `;
+                } if (("coverImage" in blog) && !("preview" in blog)) {
+                    blogPost.innerHTML = `
+                        <h3>${blog.title}</h3>
+                        <img src="${blog.coverImage}" alt="${blog.coverImage}" style="max-height: 200px; min-width:100px; height: auto;"/>
+                        <p style="color:#999999;">${blog.date}</p>
+                    `;
+                } if (!("coverImage" in blog) && !("preview" in blog)) {
+                    blogPost.innerHTML = `
+                        <h3>${blog.title}</h3>
+                        <p style="color:#999999;">${blog.date}</p>
+                    `;
+                }
+
+                if (("special" in blog)) {
+                    if ("hidden" in blog.special) {
+                        if (blog.special.hidden) {
+                            console.log(`<log> id为[${blog.id}]的BLOG被隐藏\n      原因:此blog中出现"hidden"的special标签`); //当blog中出现"hidden"的special标签,在列表中不显示此条blog(但可以用链接访问)
+                            return; //跳出本次forEach循环
+                        }
+                    }
+                    if ("password" in blog.special) {
+                        blogPost.innerHTML += "\n<span style='color:#ff5e5e;border: 1px solid #ff5e5e;background-color:hsla(0, 100%, 90%, 0.6);border-radius:4px;'>需要秘钥</span>"
+                    }
+                    if ("tag" in blog.special) {
+                        const tag = document.createElement('span');
+                        tag.textContent = blog.special.tag[0];
+                        tag.style.position = 'absolute';
+                        tag.style.top = '-4px';
+                        tag.style.right = '-4px';
+                        tag.style.backgroundColor = blog.special.tag[1];
+                        tag.style.color = blog.special.tag[2];
+                        tag.style.padding = '4px';
+                        tag.style.borderRadius = '4px';
+                        tag.style.zIndex = '10';
+                        blogPost.style.position = 'relative';
+                        blogPost.appendChild(tag);
+                    }
+                    if ("collection" in blog.special) {
+                        blogPost.addEventListener('click', () => {
+                            window.location.href = `?json=${blog.special.collection}`;
+                        });
+                        blogContent.appendChild(blogPost);
+                        return; //跳出本次forEach循环
+                    }
+                }
+
+                if (urlParams.get('json')) {
+                    blogPost.addEventListener('click', () => {
+                        window.location.href = `blog.html?json=${json_url}&id=${blog.id}`;
+                    });
+                } else {
+                    blogPost.addEventListener('click', () => {
+                        window.location.href = `blog.html?id=${blog.id}`;
+                    });
+                }
+
+                blogContent.appendChild(blogPost);
+            });
+
+            const blogEnd = document.createElement('div');
+            blogEnd.innerHTML = "- 再怎么找也没有啦 -";
+            blogEnd.style.textAlign = "center";
+            blogEnd.style.color = "#aaa";
+            blogEnd.style.paddingBottom = "10px";
+            blogContent.appendChild(blogEnd);
+        })
+        .catch(error => {
+            console.error('加载BLOG数据异常:', error);
+            document.getElementById('blog-content').innerHTML = `<p>加载BLOG数据异常[${error}]</p><br><span>尝试<a href="index.html">返回主页</a>或看看控制台有没有报错?</span>`;
+        });
+});
---
+++ js/loadblog.js
@@ -0,0 +1,62 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const blogId = urlParams.get('id');
+
+    if (urlParams.get('json')) {
+        var json_url = urlParams.get('json')
+        const back = document.getElementById('back');
+        back.href = `index.html?json=${json_url}`;
+    } else {
+        var json_url = 'json/blog.json';
+    }
+
+    console.log(json_url);
+
+    fetch(json_url)
+        .then(response => response.json())
+        .then(data => {
+            if (blogId.slice(0, 7) == "[COUNT]") {
+                // 当输入id为"[COUNT]+数字x"时,则跳转到此blog集的第x项
+                var blog = data.blogs[Number(blogId.slice(7,))];
+            }else{
+                var blog = data.blogs.find(b => b.id === blogId);
+            }
+            const blogDetail = document.getElementById('blog-detail');
+
+            if (blog) {
+                document.title = `blog详情-${blog.title}`;
+
+                if ("special" in blog) {
+                    if ("password" in blog.special) {
+                        var password = prompt("此条BLOG需要秘钥：");
+                        if (password != null) {
+                            if (password != blog.special.password) {
+                                alert("秘钥错误,点击返回");
+                                window.location.href = "index.html";//输入密码错误时重定向
+                            }
+                        } else {
+                            window.location.href = "index.html";//取消输入时重定向
+                        }
+                    }
+                }
+
+                const converter = new showdown.Converter()
+
+                blogDetail.innerHTML = `
+                        <h2>${blog.title}</h2>
+                        <p style="color:#ddd;">${blog.date}</p>
+                    `;
+
+                blog.content.forEach(line => {
+                    const htmlLine = converter.makeHtml(line);
+                    blogDetail.innerHTML += `\n${htmlLine}`;
+                });
+            } else {
+                blogDetail.innerHTML = '<h1>404 NOT FOUND</h1><p>BLOG内容未找到。</p><a href="index.html">返回主页</a></span>';
+            }
+        })
+        .catch(error => {
+            console.error('Error loading blog data:', error);
+            document.getElementById('blog-detail').innerHTML = `<p>无法加载BLOG内容。[${error}]</p><br><span>尝试<a href="index.html">返回主页</a>或看看控制台有没有报错?</span>`;
+        });
+});
