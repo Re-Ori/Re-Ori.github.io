97638b0a10e80881138f22309d4ae0e7eb52cd14
Origin <328535835@qq.com>
2024-10-17 22:05:25 +0800

    Update 0.0.7

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+// 读取config.json文件,给导航栏(document.querySelector("body > div > div.header"))添加链接
+fetch('config.json')
+    .then(response => response.json())
+    .then(data => {
+        const nav = document.querySelector("body > div > div.header");
+        console.log(data.nav);
+        data.nav.forEach(item => {
+            const link = document.createElement("a");
+            link.href = item.url;
+            link.textContent = item.name;
+            link.classList.add("a-no-style");
+
+            // 去除下划线,从左到右排列在右侧,上下居中,不换行
+            link.style.textDecoration = "none";
+            link.style.padding = "0 10px";
+            link.style.fontSize = "16px";
+            link.style.lineHeight = "40px";
+            link.style.verticalAlign = "middle";
+            link.style.whiteSpace = "nowrap";
+            // 鼠标悬停时添加下划线
+            link.addEventListener("mouseover", () => {
+                link.style.textDecoration = "underline";
+            });
+            // 鼠标离开时移除下划线
+            link.addEventListener("mouseout", () => {
+                link.style.textDecoration = "none";
+            });
+
+            nav.appendChild(link);
+        });
+    })
+
+
 // 读取config.json文件,将document.querySelector("body > div > span > a:nth-child(1)")替换为config.json中的version值
 fetch('config.json')
     .then(response => response.json())
@@ -7,19 +40,21 @@ fetch('config.json')
     })
     .catch(error => console.error('Error loading the JSON file:', error));
 
-// 每0.2s将所有未加载完成的图片添加模糊效果,加载完成的移除效果,持续100次
-var count=100;
+// 每0.2s将所有未加载完成的图片添加模糊效果和灰白条纹背景,加载完成的移除效果,持续100次
+var count = 100;
 setInterval(() => {
     const images = document.querySelectorAll('img');
     images.forEach(image => {
         if (!image.complete) {
             image.style.filter = 'blur(5px)';
+            image.style.backgroundImage = 'repeating-linear-gradient(-45deg, #aaaaaa 0px, #aaaaaa 20px, #ffffff 20px, #ffffff 40px)';
         } else {
             image.style.filter = 'blur(0px)';
+            image.style.backgroundImage = 'none';
         }
     });
     count--;
-    if (count==0) {
+    if (count == 0) {
         clearInterval();
     }
 }, 200);
